3cc3a36437af23fe75c7e57084a5c3ed9105c671
Kevin Jump <kevin@jumoo.co.uk>
2020-03-18 11:26:30 +0000

    Update to create the package version.

--- HealthCheck.Site/Properties/AssemblyInfo.cs
+++ HealthCheck.Site/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jumoo.CdfHealthChecker")]
+[assembly: AssemblyTitle("HealthCheck.Site")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jumoo.CdfHealthChecker")]
-[assembly: AssemblyCopyright("Copyright ©  2016-17")]
+[assembly: AssemblyProduct("HealthCheck.Site")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("51a260ac-36fe-4861-862e-684555f66e61")]
+[assembly: Guid("a9537314-a67d-4807-81ce-f63f9df5dbf5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
